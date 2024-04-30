d97dbfb24c8cb40c99d9efe891aa5f012a055c73
RenaldasZ <spikeris2@gmail.com>
2024-04-30 15:13:04 +0300

    recreated task_2_test for more accurate testing

--- task_2_test.py
+++ task_2_test.py
@@ -1,38 +1,43 @@
 import unittest
+from task_2 import read_color_data, calculate_color_durations
 import datetime
 
-def calculate_color_durations(filename):
-    color_durations = {'Red': datetime.timedelta(), 'Yellow': datetime.timedelta(), 'Green': datetime.timedelta()}
-    
-    with open(filename, 'r') as file:
-        next(file)
-        for line in file:
-            red, yellow, green, time_active_str, *_ = line.strip().split(',')
-            time_active = datetime.timedelta(seconds=int(time_active_str))
-            
-            if int(red):
-                color_durations['Red'] += time_active
-            elif int(yellow):
-                color_durations['Yellow'] += time_active
-            elif int(green):
-                color_durations['Green'] += time_active
-    
-    return color_durations
-
 class TestColorDurations(unittest.TestCase):
+    def setUp(self):
+        self.file_path = 'data.txt'
+
+    def test_read_color_data(self):
+        color_data = read_color_data(self.file_path)
+        self.assertIsInstance(color_data, list)
+        self.assertTrue(len(color_data) > 0)
+
     def test_calculate_color_durations(self):
-        expected_durations = {
-            'Red': datetime.timedelta(seconds=20118),
-            'Yellow': datetime.timedelta(seconds=36907),
-            'Green': datetime.timedelta(seconds=18194)
-        }
-        actual_durations = calculate_color_durations('data.txt')
-        self.assertEqual(expected_durations, actual_durations)
+        color_data = [('1', '0', '0', '3'), ('0', '1', '0', '9'), ('0', '0', '1', '1')]
+        color_durations = calculate_color_durations(color_data)
+        self.assertEqual(color_durations['Red'], datetime.timedelta(seconds=3))
+        self.assertEqual(color_durations['Yellow'], datetime.timedelta(seconds=9))
+        self.assertEqual(color_durations['Green'], datetime.timedelta(seconds=1))
+
+    def test_calculate_color_durations_multiple_entries(self):
+        color_data = [('1', '0', '0', '3'), ('0', '1', '0', '9'), ('0', '0', '1', '1'), ('1', '0', '0', '4')]
+        color_durations = calculate_color_durations(color_data)
+        self.assertEqual(color_durations['Red'], datetime.timedelta(seconds=7))
+        self.assertEqual(color_durations['Yellow'], datetime.timedelta(seconds=9))
+        self.assertEqual(color_durations['Green'], datetime.timedelta(seconds=1))
+
+    def test_calculate_color_durations_empty_data(self):
+        color_data = []
+        color_durations = calculate_color_durations(color_data)
+        self.assertEqual(color_durations['Red'], datetime.timedelta(seconds=0))
+        self.assertEqual(color_durations['Yellow'], datetime.timedelta(seconds=0))
+        self.assertEqual(color_durations['Green'], datetime.timedelta(seconds=0))
+
+    def test_calculate_color_durations_no_red(self):
+        color_data = [('0', '1', '0', '5'), ('0', '0', '1', '2')]
+        color_durations = calculate_color_durations(color_data)
+        self.assertEqual(color_durations['Red'], datetime.timedelta(seconds=0))
+        self.assertEqual(color_durations['Yellow'], datetime.timedelta(seconds=5))
+        self.assertEqual(color_durations['Green'], datetime.timedelta(seconds=2))
 
 if __name__ == '__main__':
     unittest.main()
-
-# ----------------------------------------------------------------------
-# Ran 1 test in 0.019s
-
-# OK
